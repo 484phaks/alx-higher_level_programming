26cbe38c3ad2871117d65fb9b208f5e6cd461e98
484phaks <bonganivusi@rocketmail.com>
2022-09-29 01:26:37 +0200

    done

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,4 @@
+#!/usr/bin/pythom3
+def magic_calculation (a,b):
+    return 98 + (a**b)
+
---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str1 = "holberton"
+str2 = "School"
+str1 = str1 + " " str2 
+print("welcome to {}! "" + str1))
+
