3c5216fcc67077a4c7e91947afa0fc70e2fb8263
484phaks <bonganivusi@rocketmail.com>
2022-09-29 02:41:26 +0200

    done

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 str1 = "holberton"
 str2 = "School"
-str1 = str1 + " " str2 
-print("welcome to {}!.format(str1)
+str1 = str1 + "" + str2
+print ("Welcome to {}".format(str1))
+
 
 
