6a0b7acfdc5cf29120348a9bcf308c8bb16d51c2
484phaks <bonganivusi@rocketmail.com>
2022-12-02 17:06:47 +0200

    vusi

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3 
 
 def uniq_add(my_list=[]):
-    if my_list is None:
-        return None
-    return sum(set(my_list))
-
-
+    result = 0 
+    for x in set(my_list):
+        result += x
+    return
