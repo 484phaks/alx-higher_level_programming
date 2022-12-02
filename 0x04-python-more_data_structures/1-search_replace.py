57ad37fbf05550b8765dc0b8aa3386182266d498
484phaks <bonganivusi@rocketmail.com>
2022-12-02 16:42:22 +0200

    dd

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,9 @@
 #!/usr/python3
 
 def search_replace(my_list, search, replace):
-    if my_list is None:
-        return None 
-    return [replace if x == search else x for x in my_list]
-
+    new_list = my_list[:]
+    for i in range (len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace 
+    return new_list 
 
